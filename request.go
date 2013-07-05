4ffe3e694b295896cc74474cd73466a2c4b462cc
Evan Huus <evan.huus@jadedpixel.com>
2013-07-05 17:13:05 -0400

    Put request key/version pairs together in a struct rather than using a map

--- request.go
+++ request.go
@@ -1,27 +1,20 @@
 package kafka
 
-type RequestType uint16
-
-const (
-	REQUEST_PRODUCE        RequestType = 0
-	REQUEST_FETCH                      = 1
-	REQUEST_OFFSET                     = 2
-	REQUEST_METADATA                   = 3
-	REQUEST_LEADER_AND_ISR             = 4
-	REQUEST_STOP_REPLICA               = 5
-	REQUEST_OFFSET_COMMIT              = 6
-	REQUEST_OFFSET_FETCH               = 7
-)
-
+type ApiKey uint16
 type ApiVersion uint16
 
-var API_VERSIONS = map[RequestType]ApiVersion{
-	REQUEST_PRODUCE:        0,
-	REQUEST_FETCH:          0,
-	REQUEST_OFFSET:         0,
-	REQUEST_METADATA:       0,
-	REQUEST_LEADER_AND_ISR: 0,
-	REQUEST_STOP_REPLICA:   0,
-	REQUEST_OFFSET_COMMIT:  0,
-	REQUEST_OFFSET_FETCH:   0,
+type API struct {
+	key     ApiKey
+	version ApiVersion
 }
+
+var (
+	REQUEST_PRODUCE        = API{0, 0}
+	REQUEST_FETCH          = API{1, 0}
+	REQUEST_OFFSET         = API{2, 0}
+	REQUEST_METADATA       = API{3, 0}
+	REQUEST_LEADER_AND_ISR = API{4, 0}
+	REQUEST_STOP_REPLICA   = API{5, 0}
+	REQUEST_OFFSET_COMMIT  = API{6, 0}
+	REQUEST_OFFSET_FETCH   = API{7, 0}
+)
