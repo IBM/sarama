44d5d67f6431785c43e7751d170be93aae6f1bb9
Evan Huus <evan.huus@jadedpixel.com>
2013-07-10 13:08:19 -0400

    Move API defs to right file, fix error condition

--- client.go
+++ client.go
@@ -1,21 +1,5 @@
 package kafka
 
-type API struct {
-	key     int16
-	version int16
-}
-
-var (
-	REQUEST_PRODUCE        = API{0, 0}
-	REQUEST_FETCH          = API{1, 0}
-	REQUEST_OFFSET         = API{2, 0}
-	REQUEST_METADATA       = API{3, 0}
-	REQUEST_LEADER_AND_ISR = API{4, 0}
-	REQUEST_STOP_REPLICA   = API{5, 0}
-	REQUEST_OFFSET_COMMIT  = API{6, 0}
-	REQUEST_OFFSET_FETCH   = API{7, 0}
-)
-
 type Client struct {
 	id      *string
 	brokers *brokerManager
@@ -25,5 +9,8 @@ func NewClient(id *string, host string, port int32) (client *Client, err error) 
 	client = new(Client)
 	client.id = id
 	client.brokers, err = newBrokerManager(host, port)
-	return client, err
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
 }
--- request.go
+++ request.go
@@ -1,5 +1,21 @@
 package kafka
 
+type API struct {
+	key     int16
+	version int16
+}
+
+var (
+	REQUEST_PRODUCE        = API{0, 0}
+	REQUEST_FETCH          = API{1, 0}
+	REQUEST_OFFSET         = API{2, 0}
+	REQUEST_METADATA       = API{3, 0}
+	REQUEST_LEADER_AND_ISR = API{4, 0}
+	REQUEST_STOP_REPLICA   = API{5, 0}
+	REQUEST_OFFSET_COMMIT  = API{6, 0}
+	REQUEST_OFFSET_FETCH   = API{7, 0}
+)
+
 type request struct {
 	api            API
 	correlation_id int32
