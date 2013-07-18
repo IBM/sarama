02ac6ca64c1e5c24926fc05bb6accd49f78ec287
Evan Huus <evan.huus@jadedpixel.com>
2013-07-18 15:52:24 -0400

    ProduceResponse decoding test

--- protocol/encoder_decoder_test.go
+++ protocol/encoder_decoder_test.go
@@ -8,12 +8,18 @@ import (
 // no actual tests, just helper functions for testing structures that
 // implement the encoder or decoder interfaces
 
-func testEncodable(t *testing.T, name string, in encoder, result []byte) {
+func testEncodable(t *testing.T, name string, in encoder, expect []byte) {
 	packet, err := encode(in)
 	if err != nil {
 		t.Error(err)
+	} else if !bytes.Equal(packet, expect) {
+		t.Error("Encoding", name, "failed\ngot ", packet, "\nwant", expect)
 	}
-	if !bytes.Equal(packet, result) {
-		t.Error("Encoding", name, "failed\ngot ", packet, "\nwant", result)
+}
+
+func testDecodable(t *testing.T, name string, out decoder, in []byte) {
+	err := decode(in, out)
+	if err != nil {
+		t.Error("Decoding", name, "failed:", err)
 	}
 }
