2f21c29d900d4501614577c2f75abc82cce1ca53
Evan Huus <evan.huus@jadedpixel.com>
2013-07-19 10:41:52 -0400

    test metadata responses and response header

---
+++ protocol/response_header_test.go
@@ -0,0 +1,21 @@
+package protocol
+
+import "testing"
+
+var (
+	responseHeaderBytes = []byte{
+		0x00, 0x00, 0x0f, 0x00,
+		0x0a, 0xbb, 0xcc, 0xff}
+)
+
+func TestResponseHeader(t *testing.T) {
+	header := responseHeader{}
+
+	testDecodable(t, "response header", &header, responseHeaderBytes)
+	if header.length != 0xf00 {
+		t.Error("Decoding header length failed, got", header.length)
+	}
+	if header.correlation_id != 0x0abbccff {
+		t.Error("Decoding header correlation id failed, got", header.correlation_id)
+	}
+}
