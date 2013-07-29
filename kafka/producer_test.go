f14659da8dbab54719725058cb1efeb8426e0625
Evan Huus <evan.huus@jadedpixel.com>
2013-07-29 11:39:07 -0400

    simple producer tests

--- kafka/producer_test.go
+++ kafka/producer_test.go
@@ -6,10 +6,10 @@ import (
 	"testing"
 )
 
-func TestSimpleConsumer(t *testing.T) {
-	masterResponses := make(chan []byte, 1)
+func TestSimpleProducer(t *testing.T) {
+	responses := make(chan []byte, 1)
 	extraResponses := make(chan []byte)
-	mockBroker := mock.NewBroker(t, masterResponses)
+	mockBroker := mock.NewBroker(t, responses)
 	mockExtra := mock.NewBroker(t, extraResponses)
 	defer mockBroker.Close()
 	defer mockExtra.Close()
@@ -30,7 +30,7 @@ func TestSimpleConsumer(t *testing.T) {
 		0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00}
 	binary.BigEndian.PutUint32(response[19:], uint32(mockExtra.Port()))
-	masterResponses <- response
+	responses <- response
 	go func() {
 		for i := 0; i < 10; i++ {
 			msg := []byte{
@@ -39,28 +39,10 @@ func TestSimpleConsumer(t *testing.T) {
 				0x00, 0x00, 0x00, 0x01,
 				0x00, 0x00, 0x00, 0x00,
 				0x00, 0x00,
-				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-				0x00, 0x00, 0x00, 0x1C,
-				// messageSet
-				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-				0x00, 0x00, 0x00, 0x10,
-				// message
-				0x23, 0x96, 0x4a, 0xf7, // CRC
-				0x00,
-				0x00,
-				0xFF, 0xFF, 0xFF, 0xFF,
-				0x00, 0x00, 0x00, 0x02, 0x00, 0xEE}
-			binary.BigEndian.PutUint64(msg[35:], uint64(i))
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+			binary.BigEndian.PutUint64(msg[23:], uint64(i))
 			extraResponses <- msg
 		}
-		extraResponses <- []byte{
-			0x00, 0x00, 0x00, 0x01,
-			0x00, 0x07, 'm', 'y', 'T', 'o', 'p', 'i', 'c',
-			0x00, 0x00, 0x00, 0x01,
-			0x00, 0x00, 0x00, 0x00,
-			0x00, 0x00,
-			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-			0x00, 0x00, 0x00, 0x00}
 	}()
 
 	client, err := NewClient("clientID", "localhost", mockBroker.Port())
@@ -68,22 +50,13 @@ func TestSimpleConsumer(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	consumer, err := NewConsumer(client, "myTopic", 0, "myConsumerGroup")
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	producer := NewProducer(client, "myTopic", &RandomPartitioner{})
 	for i := 0; i < 10; i++ {
-		select {
-		case msg := <-consumer.Messages():
-			if msg.Offset != int64(i) {
-				t.Error("Incorrect message offset!")
-			}
-		case err := <-consumer.Errors():
+		err = producer.SendMessage(nil, StringEncoder("ABC THE MESSAGE"))
+		if err != nil {
 			t.Error(err)
 		}
 	}
 
-	consumer.Close()
 	client.Close()
 }
