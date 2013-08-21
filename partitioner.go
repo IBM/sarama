14dc1b2c4cc0aa06bd763ef0c49f5f8404322deb
Evan Huus <evan.huus@jadedpixel.com>
2013-08-21 15:11:02 -0400

    Implement a hashing partitioner

--- partitioner.go
+++ partitioner.go
@@ -1,6 +1,9 @@
 package sarama
 
-import "math/rand"
+import (
+	"hash/crc32"
+	"math/rand"
+)
 
 // Partitioner is anything that, given a Kafka message key and a number of partitions indexed [0...numPartitions-1],
 // decides to which partition to send the message. RandomPartitioner and RoundRobinPartitioner are the
@@ -29,4 +32,21 @@ func (p *RoundRobinPartitioner) Partition(key Encoder, numPartitions int) int {
 	ret := p.partition
 	p.partition++
 	return ret
+}
+
+// HashPartitioner implements the Partitioner interface. If the key is nil, or fails to encode, then a random partition
+// is chosen. Otherwise the CRC32 of the encoded bytes is used modulus the number of partitions. This ensures that messages
+// with the same key always end up on the same partition.
+type HashPartitioner struct {
+}
+
+func (p HashPartitioner) Partition(key Encoder, numPartitions int) int {
+	if key == nil {
+		return rand.Intn(numPartitions)
+	}
+	bytes, err := key.Encode()
+	if err != nil {
+		return rand.Intn(numPartitions)
+	}
+	return int(crc32.ChecksumIEEE(bytes)) % numPartitions
 }
