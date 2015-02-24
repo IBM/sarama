f88167c1ae17bc77b7911df809bcb0545581fbf6
Willem van Bergen <willem@vanbergen.org>
2015-02-24 04:43:30 -0500

    Use the -daemon command line argument when starting zookeeper and kafka.

--- vagrant/boot_cluster.sh
+++ vagrant/boot_cluster.sh
@@ -4,10 +4,10 @@ set -ex
 
 for i in 1 2 3 4 5; do
     KAFKA_PORT=`expr $i + 6666`
-    nohup bash -c "cd ${KAFKA_INSTALL_ROOT}/kafka-${KAFKA_PORT} && bin/zookeeper-server-start.sh config/zookeeper.properties &"
+    cd ${KAFKA_INSTALL_ROOT}/kafka-${KAFKA_PORT} && bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
 done
 
 for i in 1 2 3 4 5; do
     KAFKA_PORT=`expr $i + 6666`
-    nohup bash -c "cd ${KAFKA_INSTALL_ROOT}/kafka-${KAFKA_PORT} && bin/kafka-server-start.sh config/server.properties &"
+    cd ${KAFKA_INSTALL_ROOT}/kafka-${KAFKA_PORT} && bin/kafka-server-start.sh -daemon config/server.properties
 done
