9b134876365fb3fcfc6c04f0a8fb39842eddec30
Evan Huus <eapache@gmail.com>
2015-03-12 17:20:28 -0400

    Put a sleep *after* the topic creation to give the cluster more time to be healthy in CI

--- vagrant/create_topics.sh
+++ vagrant/create_topics.sh
@@ -2,8 +2,10 @@
 
 set -ex
 
-sleep 12
+sleep 10
 
 cd ${KAFKA_INSTALL_ROOT}/kafka-6667
 bin/kafka-topics.sh --create --partitions 1 --replication-factor 3 --topic single_partition --zookeeper localhost:2181
 bin/kafka-topics.sh --create --partitions 2 --replication-factor 3 --topic multi_partition  --zookeeper localhost:2181
+
+sleep 5
