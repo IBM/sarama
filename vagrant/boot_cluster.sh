dc71c4c841e9d3b49eac1cbf8bcecbd1cdac0405
Evan Huus <eapache@gmail.com>
2015-03-25 14:37:25 -0400

    Minor tweaks to CI configuration
    - provide more time for booting the cluster
    - use recent point releases of golang rather than the original 1.x.0 releases

--- vagrant/boot_cluster.sh
+++ vagrant/boot_cluster.sh
@@ -4,10 +4,14 @@ set -ex
 
 vagrant/run_toxiproxy.sh &
 
+sleep 5
+
 for i in 1 2 3 4 5; do
     KAFKA_PORT=`expr $i + 9090`
     cd ${KAFKA_INSTALL_ROOT}/kafka-${KAFKA_PORT} && bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
 done
+
+sleep 5
 
 for i in 1 2 3 4 5; do
     KAFKA_PORT=`expr $i + 9090`
