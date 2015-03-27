bfef47464551bce38c81d0bf174ba93b2acd3d7a
Evan Huus <eapache@gmail.com>
2015-03-27 14:37:26 -0400

    Instead of sleeping, poll for the socket to be ready
    Hopefully this fixes the heisenbug where we try to set up the topics before the
    cluster has actually booted.

--- vagrant/create_topics.sh
+++ vagrant/create_topics.sh
@@ -2,7 +2,8 @@
 
 set -ex
 
-sleep 10
+while ! nc -q 1 localhost 21801 </dev/null; do echo "Waiting"; sleep 1; done
+while ! nc -q 1 localhost 29092 </dev/null; do echo "Waiting"; sleep 1; done
 
 cd ${KAFKA_INSTALL_ROOT}/kafka-9092
 bin/kafka-topics.sh --create --partitions 1 --replication-factor 3 --topic single_partition --zookeeper localhost:2181
