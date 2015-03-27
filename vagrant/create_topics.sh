215604bb200105b3419803c6fc1d4a85ae3e75b9
Evan Huus <eapache@gmail.com>
2015-03-27 14:47:19 -0400

    Remove all cluster sleeps in favour of polling

--- vagrant/create_topics.sh
+++ vagrant/create_topics.sh
@@ -8,5 +8,3 @@ while ! nc -q 1 localhost 29092 </dev/null; do echo "Waiting"; sleep 1; done
 cd ${KAFKA_INSTALL_ROOT}/kafka-9092
 bin/kafka-topics.sh --create --partitions 1 --replication-factor 3 --topic single_partition --zookeeper localhost:2181
 bin/kafka-topics.sh --create --partitions 2 --replication-factor 3 --topic multi_partition  --zookeeper localhost:2181
-
-sleep 5
--- vagrant/run_toxiproxy.sh
+++ vagrant/run_toxiproxy.sh
@@ -5,7 +5,7 @@ set -ex
 ${KAFKA_INSTALL_ROOT}/toxiproxy -port 8474 -host 0.0.0.0 &
 PID=$!
 
-sleep 5
+while ! nc -q 1 localhost 8474 </dev/null; do echo "Waiting"; sleep 1; done
 
 wget -S --post-data='{"name":"zk1", "upstream":"localhost:21801", "listen":"0.0.0.0:2181"}' localhost:8474/proxies
 wget -S --post-data='{"name":"zk2", "upstream":"localhost:21802", "listen":"0.0.0.0:2182"}' localhost:8474/proxies
