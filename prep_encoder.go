66411d8222087377937c6f176dbd4f95f32d821e
Evan Huus <eapache@gmail.com>
2015-07-29 14:00:12 -0400

    Cleanup a bunch more error formats

--- prep_encoder.go
+++ prep_encoder.go
@@ -29,7 +29,7 @@ func (pe *prepEncoder) putInt64(in int64) {
 
 func (pe *prepEncoder) putArrayLength(in int) error {
 	if in > math.MaxInt32 {
-		return PacketEncodingError{fmt.Sprintf("Array too long: %d", in)}
+		return PacketEncodingError{fmt.Sprintf("array too long (%d)", in)}
 	}
 	pe.length += 4
 	return nil
@@ -43,7 +43,7 @@ func (pe *prepEncoder) putBytes(in []byte) error {
 		return nil
 	}
 	if len(in) > math.MaxInt32 {
-		return PacketEncodingError{fmt.Sprintf("Byteslice too long: %d", len(in))}
+		return PacketEncodingError{fmt.Sprintf("byteslice too long (%d)", len(in))}
 	}
 	pe.length += len(in)
 	return nil
@@ -51,7 +51,7 @@ func (pe *prepEncoder) putBytes(in []byte) error {
 
 func (pe *prepEncoder) putRawBytes(in []byte) error {
 	if len(in) > math.MaxInt32 {
-		return PacketEncodingError{fmt.Sprintf("Byteslice too long: %d", len(in))}
+		return PacketEncodingError{fmt.Sprintf("byteslice too long (%d)", len(in))}
 	}
 	pe.length += len(in)
 	return nil
@@ -60,7 +60,7 @@ func (pe *prepEncoder) putRawBytes(in []byte) error {
 func (pe *prepEncoder) putString(in string) error {
 	pe.length += 2
 	if len(in) > math.MaxInt16 {
-		return PacketEncodingError{fmt.Sprintf("String too long: %d", len(in))}
+		return PacketEncodingError{fmt.Sprintf("string too long (%d)", len(in))}
 	}
 	pe.length += len(in)
 	return nil
