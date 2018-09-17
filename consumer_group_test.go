ed8fd5418142ae0cf27b3e8e64cd779cc30fb932
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2018-09-17 12:54:35 +0100

    Better documentation and examples

--- consumer_group_test.go
+++ consumer_group_test.go
@@ -5,16 +5,20 @@ import (
 	"fmt"
 )
 
-type exampleConsumerGroupHandler func(sess ConsumerGroupSession, claim ConsumerGroupClaim) error
+type exampleConsumerGroupHandler struct{}
 
-func (h exampleConsumerGroupHandler) Setup(_ ConsumerGroupSession) error   { return nil }
-func (h exampleConsumerGroupHandler) Cleanup(_ ConsumerGroupSession) error { return nil }
-func (h exampleConsumerGroupHandler) ConsumeClaim(s ConsumerGroupSession, c ConsumerGroupClaim) error {
-	return h(s, c)
+func (exampleConsumerGroupHandler) Setup(_ ConsumerGroupSession) error   { return nil }
+func (exampleConsumerGroupHandler) Cleanup(_ ConsumerGroupSession) error { return nil }
+func (h exampleConsumerGroupHandler) ConsumeClaim(sess ConsumerGroupSession, claim ConsumerGroupClaim) error {
+	for msg := range claim.Messages() {
+		fmt.Printf("Message topic:%q partition:%d offset:%d\n", msg.Topic, msg.Partition, msg.Offset)
+		sess.MarkMessage(msg, "")
+	}
+	return nil
 }
 
 func ExampleConsumerGroup() {
-	// Init config, specify version
+	// Init config, specify appropriate version
 	config := NewConfig()
 	config.Version = V1_0_0_0
 	config.Consumer.Return.Errors = true
@@ -43,13 +47,10 @@ func ExampleConsumerGroup() {
 	// Iterate over consumer sessions.
 	ctx := context.Background()
 	for {
-		err := group.Consume(ctx, []string{"my-topic"}, exampleConsumerGroupHandler(func(sess ConsumerGroupSession, claim ConsumerGroupClaim) error {
-			for msg := range claim.Messages() {
-				fmt.Printf("Message topic:%q partition:%d offset:%d\n", msg.Topic, msg.Partition, msg.Offset)
-				sess.MarkMessage(msg, "")
-			}
-			return nil
-		}))
+		topics := []string{"my-topic"}
+		handler := exampleConsumerGroupHandler{}
+
+		err := group.Consume(ctx, topics, handler)
 		if err != nil {
 			panic(err)
 		}
