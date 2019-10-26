e1dcddde5505b7cb9f7f09b489935f11baef9398
unknown <weeco91@gmail.com>
2019-10-26 12:50:10 +0200

    Fix comment

--- describe_log_dirs_request_test.go
+++ describe_log_dirs_request_test.go
@@ -6,7 +6,7 @@ var (
 	emptyDescribeLogDirsRequest = []byte{255, 255, 255, 255} // Empty array (array length -1 sent)
 	topicDescribeLogDirsRequest = []byte{
 		0, 0, 0, 1, // DescribeTopics array, Array length 1
-		0, 6, // Topic name length 18
+		0, 6, // Topic name length 6
 		'r', 'a', 'n', 'd', 'o', 'm', // Topic name
 		0, 0, 0, 2, // PartitionIDs int32 array, Array length 2
 		0, 0, 0, 25, // PartitionID 25
