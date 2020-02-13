675d2830688a34a75b7ee27d271cb6452f1f01f6
Dirk Wilden <dirk.wilden@device-insight.com>
2020-02-13 14:09:22 +0100

    add test for alter_partition_reassignments_response

--- alter_partition_reassignments_response_test.go
+++ alter_partition_reassignments_response_test.go
@@ -10,6 +10,19 @@ var (
 		1, // empty errors array
 		0, // empty tagged fields
 	}
+
+	alterPartitionReassignmentsResponseWithError = []byte{
+		0, 0, 39, 16, // ThrottleTimeMs 10000
+		0, 12, // errorcode
+		6, 101, 114, 114, 111, 114, // error string "error"
+		2,                         // errors array length 1
+		6, 116, 111, 112, 105, 99, // topic name "topic"
+		2,          // partition array length 1
+		0, 0, 0, 1, // partitionId
+		0, 3, //kerror
+		7, 101, 114, 114, 111, 114, 50, // error string "error2"
+		0, 0, 0, // empty tagged fields
+	}
 )
 
 func TestAlterPartitionReassignmentResponse(t *testing.T) {
@@ -21,4 +34,12 @@ func TestAlterPartitionReassignmentResponse(t *testing.T) {
 	}
 
 	testResponse(t, "no error", response, alterPartitionReassignmentsResponseNoError)
+
+	errorMessage := "error"
+	partitionError := "error2"
+	response.ErrorCode = 12
+	response.ErrorMessage = &errorMessage
+	response.AddError("topic", 1, 3, &partitionError)
+
+	testResponse(t, "with error", response, alterPartitionReassignmentsResponseWithError)
 }
