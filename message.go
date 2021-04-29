f29bbec50302b262ad68ee898c555f9898aef317
bestgopher <84328409@qq.com>
2021-04-29 08:04:00 +0800

    decompress: fix error

--- message.go
+++ message.go
@@ -146,7 +146,7 @@ func (m *Message) decode(pd packetDecoder) (err error) {
 	// for future metrics about the compression ratio in fetch requests
 	m.compressedSize = len(m.Value)
 
-	if m.Value != nil && m.Codec == CompressionNone {
+	if m.Value != nil && m.Codec != CompressionNone {
 		m.Value, err = decompress(m.Codec, m.Value)
 		if err != nil {
 			return err
