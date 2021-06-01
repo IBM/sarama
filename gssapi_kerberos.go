a51d20aa5788d0930d43013e03d735a6869ad7c9
Dominic Evans <dominic.evans@uk.ibm.com>
2021-06-01 23:41:06 +0100

    fix: cap kerberos payload at int32

--- gssapi_kerberos.go
+++ gssapi_kerberos.go
@@ -59,8 +59,8 @@ type KerberosClient interface {
 func (krbAuth *GSSAPIKerberosAuth) writePackage(broker *Broker, payload []byte) (int, error) {
 	length := uint64(len(payload))
 	size := length + 4 // 4 byte length header + payload
-	if size > math.MaxUint32 {
-		return 0, errors.New("payload too large, will overflow uint32")
+	if size > math.MaxInt32 {
+		return 0, errors.New("payload too large, will overflow int32")
 	}
 	finalPackage := make([]byte, size)
 	copy(finalPackage[4:], payload)
