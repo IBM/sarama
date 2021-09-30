c9e413163323e4138f4e8f0e545220ccc3aedfb9
Liz Fong-Jones <lizf@honeycomb.io>
2021-09-30 16:37:43 -0700

    pass level param through

--- zstd.go
+++ zstd.go
@@ -1,18 +1,37 @@
 package sarama
 
 import (
+	"sync"
+
 	"github.com/klauspost/compress/zstd"
 )
 
+var zstdEncMap sync.Map
+
 var (
 	zstdDec, _ = zstd.NewReader(nil)
-	zstdEnc, _ = zstd.NewWriter(nil, zstd.WithZeroFrames(true))
 )
+
+func getEncoder(level int) *zstd.Encoder {
+	if ret, ok := zstdEncMap.Load(level); ok {
+		return ret.(*zstd.Encoder)
+	}
+	// It's possible to race and create multiple new writers.
+	// Only one will survive GC after use.
+	encoderLevel := zstd.SpeedDefault
+	if level != CompressionLevelDefault {
+		encoderLevel = zstd.EncoderLevelFromZstd(level)
+	}
+	zstdEnc, _ := zstd.NewWriter(nil, zstd.WithZeroFrames(true),
+		zstd.WithEncoderLevel(encoderLevel))
+	zstdEncMap.Store(level, zstdEnc)
+	return zstdEnc
+}
 
 func zstdDecompress(dst, src []byte) ([]byte, error) {
 	return zstdDec.DecodeAll(src, dst)
 }
 
-func zstdCompress(dst, src []byte) ([]byte, error) {
-	return zstdEnc.EncodeAll(src, dst), nil
+func zstdCompress(level int, dst, src []byte) ([]byte, error) {
+	return getEncoder(level).EncodeAll(src, dst), nil
 }
