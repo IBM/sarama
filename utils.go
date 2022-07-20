b12642e20f02b3017ba2a73fad1a6849d6b0d539
Dominic Evans <dominic.evans@uk.ibm.com>
2022-07-21 00:35:09 +0100

    chore: add and test against kafka 3.2.0

--- utils.go
+++ utils.go
@@ -182,6 +182,7 @@ var (
 	V3_0_0_0  = newKafkaVersion(3, 0, 0, 0)
 	V3_0_1_0  = newKafkaVersion(3, 0, 1, 0)
 	V3_1_0_0  = newKafkaVersion(3, 1, 0, 0)
+	V3_2_0_0  = newKafkaVersion(3, 2, 0, 0)
 
 	SupportedVersions = []KafkaVersion{
 		V0_8_2_0,
@@ -227,9 +228,10 @@ var (
 		V3_0_0_0,
 		V3_0_1_0,
 		V3_1_0_0,
+		V3_2_0_0,
 	}
 	MinVersion     = V0_8_2_0
-	MaxVersion     = V3_1_0_0
+	MaxVersion     = V3_2_0_0
 	DefaultVersion = V1_0_0_0
 )
 
