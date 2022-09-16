39c350b93f1cc0a16fc4050a2db52d52fcf44083
auntan <spawn273@gmail.com>
2022-09-16 18:51:31 +0500

    fix: prevent metrics leak with cleanup Keep track all components metrics and unregister them on close

--- produce_set.go
+++ produce_set.go
@@ -181,7 +181,7 @@ func (ps *produceSet) buildRequest() *ProduceRequest {
 						msg.Offset = int64(i)
 					}
 				}
-				payload, err := encode(set.recordsToSend.MsgSet, ps.parent.conf.MetricRegistry)
+				payload, err := encode(set.recordsToSend.MsgSet, ps.parent.metricsRegistry)
 				if err != nil {
 					Logger.Println(err) // if this happens, it's basically our fault.
 					panic(err)
