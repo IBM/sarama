7dbf0b5d85538641227a30a281f1a54e18a94cb7
faillefer <23032941+faillefer@users.noreply.github.com>
2023-03-07 22:49:17 +0100

    fix: use version 4 of DescribeGroupsRequest only if kafka broker version is >= 2.4
    Fixes #2443

--- describe_groups_response.go
+++ describe_groups_response.go
@@ -65,8 +65,14 @@ func (r *DescribeGroupsResponse) headerVersion() int16 {
 
 func (r *DescribeGroupsResponse) requiredVersion() KafkaVersion {
 	switch r.Version {
-	case 1, 2, 3, 4:
+	case 1:
+		return V1_1_0_0
+	case 2:
+		return V2_0_0_0
+	case 3:
 		return V2_3_0_0
+	case 4:
+		return V2_4_0_0
 	}
 	return V0_9_0_0
 }
