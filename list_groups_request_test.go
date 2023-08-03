fb761f2fd853ad0fe1338674e9b3c739b94cb751
Adrian Preston <prestona@users.noreply.github.com>
2023-08-03 12:40:38 +0100

    feat: support up to v4 of the ListGroups API (#2541)
    Signed-off-by: Adrian Preston <PRESTONA@uk.ibm.com>

--- list_groups_request_test.go
+++ list_groups_request_test.go
@@ -4,4 +4,34 @@ import "testing"
 
 func TestListGroupsRequest(t *testing.T) {
 	testRequest(t, "ListGroupsRequest", &ListGroupsRequest{}, []byte{})
+
+	testRequest(t, "ListGroupsRequest", &ListGroupsRequest{
+		Version: 1,
+	}, []byte{})
+
+	testRequest(t, "ListGroupsRequest", &ListGroupsRequest{
+		Version: 2,
+	}, []byte{})
+
+	testRequest(t, "ListGroupsRequest", &ListGroupsRequest{
+		Version: 3,
+	}, []byte{
+		0, //		0, // empty tag buffer
+	})
+
+	testRequest(t, "ListGroupsRequest", &ListGroupsRequest{
+		Version: 4,
+	}, []byte{
+		1, // compact array length (0)
+		0, // empty tag buffer
+	})
+
+	testRequest(t, "ListGroupsRequest", &ListGroupsRequest{
+		Version:      4,
+		StatesFilter: []string{"Empty"},
+	}, []byte{
+		2,                          // compact array length (1)
+		6, 'E', 'm', 'p', 't', 'y', // compact string
+		0, // empty tag buffer
+	})
 }
--- list_groups_response_test.go
+++ list_groups_response_test.go
@@ -22,6 +22,17 @@ var (
 		0, 3, 'f', 'o', 'o', // group name
 		0, 8, 'c', 'o', 'n', 's', 'u', 'm', 'e', 'r', // protocol type
 	}
+
+	listGroupResponseV4 = []byte{
+		0, 0, 0, 0, // no throttle time
+		0, 0, // no error
+		2,                // compact array length (1)
+		4, 'f', 'o', 'o', // group name (compact string)
+		9, 'c', 'o', 'n', 's', 'u', 'm', 'e', 'r', // protocol type (compact string)
+		6, 'E', 'm', 'p', 't', 'y', // state (compact string)
+		0, // Empty tag buffer
+		0, // Empty tag buffer
+	}
 )
 
 func TestListGroupsResponse(t *testing.T) {
@@ -55,5 +66,20 @@ func TestListGroupsResponse(t *testing.T) {
 	}
 	if response.Groups["foo"] != "consumer" {
 		t.Error("Expected foo group to use consumer protocol")
+	}
+
+	response = new(ListGroupsResponse)
+	testVersionDecodable(t, "no error", response, listGroupResponseV4, 4)
+	if !errors.Is(response.Err, ErrNoError) {
+		t.Error("Expected no gerror, found:", response.Err)
+	}
+	if len(response.Groups) != 1 {
+		t.Error("Expected one group")
+	}
+	if response.Groups["foo"] != "consumer" {
+		t.Error("Expected foo group to use consumer protocol")
+	}
+	if response.GroupsData["foo"].GroupState != "Empty" {
+		t.Error("Expected foo grup to have empty state")
 	}
 }
