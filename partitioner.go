1e3e0960c1d7737577e0efab2bd7258e60039ccf
csm8118 <csm8118@yahoo.com>
2023-08-04 17:27:19 +0000

    bug: implement unsigned modulus for partitioning with crc32 hashing
    Signed-off-by: csm8118 <csm8118@yahoo.com>

--- partitioner.go
+++ partitioner.go
@@ -2,6 +2,7 @@ package sarama
 
 import (
 	"hash"
+	"hash/crc32"
 	"hash/fnv"
 	"math/rand"
 	"time"
@@ -50,6 +51,15 @@ type HashPartitionerOption func(*hashPartitioner)
 func WithAbsFirst() HashPartitionerOption {
 	return func(hp *hashPartitioner) {
 		hp.referenceAbs = true
+	}
+}
+
+// WithHashUnsigned means the partitioner treats the hashed value as unsigned when
+// partitioning.  This is intended to be combined with the crc32 hash algorithm to
+// be compatible with librdkafka's implementation
+func WithHashUnsigned() HashPartitionerOption {
+	return func(hp *hashPartitioner) {
+		hp.hashUnsigned = true
 	}
 }
 
@@ -126,6 +136,7 @@ type hashPartitioner struct {
 	random       Partitioner
 	hasher       hash.Hash32
 	referenceAbs bool
+	hashUnsigned bool
 }
 
 // NewCustomHashPartitioner is a wrapper around NewHashPartitioner, allowing the use of custom hasher.
@@ -137,6 +148,7 @@ func NewCustomHashPartitioner(hasher func() hash.Hash32) PartitionerConstructor 
 		p.random = NewRandomPartitioner(topic)
 		p.hasher = hasher()
 		p.referenceAbs = false
+		p.hashUnsigned = false
 		return p
 	}
 }
@@ -148,6 +160,7 @@ func NewCustomPartitioner(options ...HashPartitionerOption) PartitionerConstruct
 		p.random = NewRandomPartitioner(topic)
 		p.hasher = fnv.New32a()
 		p.referenceAbs = false
+		p.hashUnsigned = false
 		for _, option := range options {
 			option(p)
 		}
@@ -164,6 +177,7 @@ func NewHashPartitioner(topic string) Partitioner {
 	p.random = NewRandomPartitioner(topic)
 	p.hasher = fnv.New32a()
 	p.referenceAbs = false
+	p.hashUnsigned = false
 	return p
 }
 
@@ -176,6 +190,19 @@ func NewReferenceHashPartitioner(topic string) Partitioner {
 	p.random = NewRandomPartitioner(topic)
 	p.hasher = fnv.New32a()
 	p.referenceAbs = true
+	p.hashUnsigned = false
+	return p
+}
+
+// NewConsistentCRCHashPartitioner is like NewHashPartitioner execpt that it uses the *unsigned* crc32 hash
+// of the encoded bytes of the message key modulus the number of partitions.  This is compatible with
+// librdkafka's `consistent_random` partitioner
+func NewConsistentCRCHashPartitioner(topic string) Partitioner {
+	p := new(hashPartitioner)
+	p.random = NewRandomPartitioner(topic)
+	p.hasher = crc32.NewIEEE()
+	p.referenceAbs = false
+	p.hashUnsigned = true
 	return p
 }
 
@@ -199,6 +226,10 @@ func (p *hashPartitioner) Partition(message *ProducerMessage, numPartitions int3
 	// but not past Sarama versions
 	if p.referenceAbs {
 		partition = (int32(p.hasher.Sum32()) & 0x7fffffff) % numPartitions
+	} else if p.hashUnsigned {
+		// librdkafka treats the hashed value as unsigned.  If `hashUnsigned` is set we are compatible
+		// with librdkafka's `consistent` partitioning but not past Sarama versions
+		partition = int32(p.hasher.Sum32() % uint32(numPartitions))
 	} else {
 		partition = int32(p.hasher.Sum32()) % numPartitions
 		if partition < 0 {
