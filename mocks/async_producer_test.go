c08d93e13dbabbe48799b2e8b3fe6805bad09c50
Dominic Evans <dominic.evans@uk.ibm.com>
2023-11-03 10:58:40 +0000

    fix(config): relax ClientID validation after 1.0.0
    The original validation regex was based on the one that existed in the
    Java clients pre-1.0.0 but KIP-190 removed the client-side validation
    because instead the brokers were updated to be able to cope with any
    clientID and sanitize it before using it in metrics etc.
    
    We can do similar in Sarama and only do client-side validation when the
    user has specified a version number older than 1.0.0
    
    Fixes #2697
    
    Signed-off-by: Dominic Evans <dominic.evans@uk.ibm.com>

--- mocks/async_producer_test.go
+++ mocks/async_producer_test.go
@@ -251,14 +251,15 @@ func (brokePartitioner) RequiresConsistency() bool { return false }
 func TestProducerWithInvalidConfiguration(t *testing.T) {
 	trm := newTestReporterMock()
 	config := NewTestConfig()
-	config.ClientID = "not a valid client ID"
+	config.Version = sarama.V0_11_0_2
+	config.ClientID = "not a valid producer ID"
 	mp := NewAsyncProducer(trm, config)
 	if err := mp.Close(); err != nil {
 		t.Error(err)
 	}
 	if len(trm.errors) != 1 {
 		t.Error("Expected to report a single error")
-	} else if !strings.Contains(trm.errors[0], "ClientID is invalid") {
+	} else if !strings.Contains(trm.errors[0], `ClientID value "not a valid producer ID" is not valid for Kafka versions before 1.0.0`) {
 		t.Errorf("Unexpected error: %s", trm.errors[0])
 	}
 }
