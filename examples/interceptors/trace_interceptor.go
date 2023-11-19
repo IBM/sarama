d09a287f30bdf73b1c9f53c3f7a4b2ecc2bd41d3
Dominic Evans <8060970+dnwe@users.noreply.github.com>
2023-11-19 14:20:33 +0000

    fix(examples): housekeeping of code and deps (#2720)
    - standardise `-version` flag for controlling Version field in
      sarama.Config and default to sarama.DefaultVersion
    - bump to mod directive of go1.18 to match parent module
    - bump otel deps to latest and update interceptor code to match API
      changes
    
    Signed-off-by: Dominic Evans <dominic.evans@uk.ibm.com>

--- examples/interceptors/trace_interceptor.go
+++ examples/interceptors/trace_interceptor.go
@@ -4,32 +4,32 @@ import (
 	"context"
 	"strings"
 
-	"go.opentelemetry.io/otel/api/global"
-	"go.opentelemetry.io/otel/api/kv"
-	"go.opentelemetry.io/otel/api/trace"
+	"go.opentelemetry.io/otel/attribute"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
 
 	"github.com/IBM/sarama"
 )
 
 type OTelInterceptor struct {
 	tracer     trace.Tracer
-	fixedAttrs []kv.KeyValue
+	fixedAttrs []attribute.KeyValue
 }
 
 // NewOTelInterceptor processes span for intercepted messages and add some
 // headers with the span data.
 func NewOTelInterceptor(brokers []string) *OTelInterceptor {
 	oi := OTelInterceptor{}
-	oi.tracer = global.TraceProvider().Tracer("shopify.com/sarama/examples/interceptors")
+	oi.tracer = sdktrace.NewTracerProvider().Tracer("github.com/IBM/sarama/examples/interceptors")
 
 	// These are based on the spec, which was reachable as of 2020-05-15
 	// https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/messaging.md
-	oi.fixedAttrs = []kv.KeyValue{
-		kv.String("messaging.destination_kind", "topic"),
-		kv.String("span.otel.kind", "PRODUCER"),
-		kv.String("messaging.system", "kafka"),
-		kv.String("net.transport", "IP.TCP"),
-		kv.String("messaging.url", strings.Join(brokers, ",")),
+	oi.fixedAttrs = []attribute.KeyValue{
+		attribute.String("messaging.destination_kind", "topic"),
+		attribute.String("span.otel.kind", "PRODUCER"),
+		attribute.String("messaging.system", "kafka"),
+		attribute.String("net.transport", "IP.TCP"),
+		attribute.String("messaging.url", strings.Join(brokers, ",")),
 	}
 	return &oi
 }
@@ -63,31 +63,28 @@ func (oi *OTelInterceptor) OnSend(msg *sarama.ProducerMessage) {
 	if shouldIgnoreMsg(msg) {
 		return
 	}
-	_ = oi.tracer.WithSpan(context.TODO(), msg.Topic,
-		func(ctx context.Context) error {
-			span := trace.SpanFromContext(ctx)
-			spanContext := span.SpanContext()
-			attWithTopic := append(
-				oi.fixedAttrs,
-				kv.String("messaging.destination", msg.Topic),
-				kv.String("messaging.message_id", spanContext.SpanID.String()),
-			)
-			span.SetAttributes(attWithTopic...)
+	_, span := oi.tracer.Start(context.TODO(), msg.Topic)
+	defer span.End()
+	spanContext := span.SpanContext()
+	attWithTopic := append(
+		oi.fixedAttrs,
+		attribute.String("messaging.destination", msg.Topic),
+		attribute.String("messaging.message_id", spanContext.SpanID().String()),
+	)
+	span.SetAttributes(attWithTopic...)
 
-			// remove existing partial tracing headers if exists
-			noTraceHeaders := msg.Headers[:0]
-			for _, h := range msg.Headers {
-				key := string(h.Key)
-				if key != TraceHeaderName && key != SpanHeaderName && key != MessageIDHeaderName {
-					noTraceHeaders = append(noTraceHeaders, h)
-				}
-			}
-			traceHeaders := []sarama.RecordHeader{
-				{Key: []byte(TraceHeaderName), Value: []byte(spanContext.TraceID.String())},
-				{Key: []byte(SpanHeaderName), Value: []byte(spanContext.SpanID.String())},
-				{Key: []byte(MessageIDHeaderName), Value: []byte(spanContext.SpanID.String())},
-			}
-			msg.Headers = append(noTraceHeaders, traceHeaders...)
-			return nil
-		})
+	// remove existing partial tracing headers if exists
+	noTraceHeaders := msg.Headers[:0]
+	for _, h := range msg.Headers {
+		key := string(h.Key)
+		if key != TraceHeaderName && key != SpanHeaderName && key != MessageIDHeaderName {
+			noTraceHeaders = append(noTraceHeaders, h)
+		}
+	}
+	traceHeaders := []sarama.RecordHeader{
+		{Key: []byte(TraceHeaderName), Value: []byte(spanContext.TraceID().String())},
+		{Key: []byte(SpanHeaderName), Value: []byte(spanContext.SpanID().String())},
+		{Key: []byte(MessageIDHeaderName), Value: []byte(spanContext.SpanID().String())},
+	}
+	msg.Headers = append(noTraceHeaders, traceHeaders...)
 }
