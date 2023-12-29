09f29cf466f97a1c663cc3b401d6b9f29edb421d
Lev Zakharov <l.j.zakharov@gmail.com>
2023-12-29 11:51:13 +0300

    fix: don't waste time for backoff on member id required error
    Signed-off-by: Lev Zakharov <l.j.zakharov@gmail.com>

--- consumer_group.go
+++ consumer_group.go
@@ -326,7 +326,7 @@ func (c *consumerGroup) newSession(ctx context.Context, topics []string, handler
 		// response and send another join request with that id to actually join the
 		// group
 		c.memberID = join.MemberId
-		return c.retryNewSession(ctx, topics, handler, retries+1 /*keep retry time*/, false)
+		return c.newSession(ctx, topics, handler, retries)
 	case ErrFencedInstancedId:
 		if c.groupInstanceId != nil {
 			Logger.Printf("JoinGroup failed: group instance id %s has been fenced\n", *c.groupInstanceId)
