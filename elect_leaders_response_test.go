1358013ce2dbdca2dacf1964b5c26c3beaa3fe1d
joey <zchengjoey@gmail.com>
2024-12-11 19:39:53 +0800

    feat(admin): implement leader election api
    Signed-off-by: joey <zchengjoey@gmail.com>

---
+++ elect_leaders_response_test.go
@@ -0,0 +1,29 @@
+package sarama
+
+import "testing"
+
+var electLeadersResponseOneTopic = []byte{
+	0, 0, 3, 232, // ThrottleTimeMs 1000
+	0, 0, // errorCode
+	2,                         // number of topics
+	6, 116, 111, 112, 105, 99, // topic name "topic"
+	2,          // number of partitions
+	0, 0, 0, 0, // partition 0
+	0, 0, // empty tagged fields
+	0, 0, // empty tagged fields
+	0, 0, // empty tagged fields
+}
+
+func TestElectLeadersResponse(t *testing.T) {
+	var response = &ElectLeadersResponse{
+		Version:        int16(2),
+		ThrottleTimeMs: int32(1000),
+		ReplicaElectionResults: map[string]map[int32]*PartitionResult{
+			"topic": {
+				0: {},
+			},
+		},
+	}
+
+	testResponse(t, "one topic", response, electLeadersResponseOneTopic)
+}
