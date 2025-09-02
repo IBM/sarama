2fb0acd962b0ca4bf2d7d01d723e7aa26f1716a8
Sahil-4555 <sahilsojitra4555@gmail.com>
2025-09-02 20:22:33 +0530

    refactor: feeback changes
    Signed-off-by: Sahil-4555 <sahilsojitra4555@gmail.com>

--- mocks/consumer.go
+++ mocks/consumer.go
@@ -398,7 +398,7 @@ func (pc *PartitionConsumer) YieldMessage(msg *sarama.ConsumerMessage) *Partitio
 	msg.Partition = pc.partition
 
 	if pc.paused {
-		msg.Offset = atomic.AddInt64(&pc.suppressedHighWaterMarkOffset, 1)
+		msg.Offset = atomic.AddInt64(&pc.suppressedHighWaterMarkOffset, 1) - 1
 		pc.suppressedMessages <- msg
 	} else {
 		msg.Offset = pc.highWaterMarkOffset.Add(1) - 1
