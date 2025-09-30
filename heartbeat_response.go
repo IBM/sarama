d13ea7572804c6a62e239a6a6dce667ce9343e1e
beanz <mark.hindess@gmail.com>
2025-09-30 17:11:04 +0100

    feat: support Heartbeat V4 protocol
    Version 4 is the first flexible version
    
    Signed-off-by: beanz <mark.hindess@gmail.com>

--- heartbeat_response.go
+++ heartbeat_response.go
@@ -17,6 +17,7 @@ func (r *HeartbeatResponse) encode(pe packetEncoder) error {
 		pe.putInt32(r.ThrottleTime)
 	}
 	pe.putKError(r.Err)
+	pe.putEmptyTaggedFieldArray()
 	return nil
 }
 
@@ -33,7 +34,8 @@ func (r *HeartbeatResponse) decode(pd packetDecoder, version int16) error {
 		return err
 	}
 
-	return nil
+	_, err = pd.getEmptyTaggedFieldArray()
+	return err
 }
 
 func (r *HeartbeatResponse) key() int16 {
@@ -45,15 +47,28 @@ func (r *HeartbeatResponse) version() int16 {
 }
 
 func (r *HeartbeatResponse) headerVersion() int16 {
+	if r.Version >= 4 {
+		return 1
+	}
 	return 0
 }
 
 func (r *HeartbeatResponse) isValidVersion() bool {
-	return r.Version >= 0 && r.Version <= 3
+	return r.Version >= 0 && r.Version <= 4
+}
+
+func (r *HeartbeatResponse) isFlexible() bool {
+	return r.isFlexibleVersion(r.Version)
+}
+
+func (r *HeartbeatResponse) isFlexibleVersion(version int16) bool {
+	return version >= 4
 }
 
 func (r *HeartbeatResponse) requiredVersion() KafkaVersion {
 	switch r.Version {
+	case 4:
+		return V2_4_0_0
 	case 3:
 		return V2_3_0_0
 	case 2:
