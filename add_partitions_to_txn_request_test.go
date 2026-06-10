eb8c5bf1f98f296d1d11566f46d5a2c493b738c6
beanz <mark.hindess@gmail.com>
2026-06-10 20:34:57 +0100

    chore: correct confusing test packet layout
    Signed-off-by: beanz <mark.hindess@gmail.com>

--- add_partitions_to_txn_request_test.go
+++ add_partitions_to_txn_request_test.go
@@ -7,8 +7,8 @@ import "testing"
 var addPartitionsToTxnRequest = []byte{
 	0, 3, 't', 'x', 'n',
 	0, 0, 0, 0, 0, 0, 31, 64, // ProducerID
-	0, 0, 0, 0, // ProducerEpoch
-	0, 1, // 1 topic
+	0, 0, // ProducerEpoch
+	0, 0, 0, 1, // 1 topic
 	0, 5, 't', 'o', 'p', 'i', 'c',
 	0, 0, 0, 1, 0, 0, 0, 1,
 }
