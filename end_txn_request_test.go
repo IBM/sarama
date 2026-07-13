5fbd35e51921a964255c909f77afb782e3e106de
Dominic Evans <dominic.evans@uk.ibm.com>
2026-07-13 16:52:05 +0200

    feat: support EndTxn v3
    Version 3 enables flexible versions (KAFKA-10547, Kafka 2.8.0).
    
    Signed-off-by: Dominic Evans <dominic.evans@uk.ibm.com>

--- end_txn_request_test.go
+++ end_txn_request_test.go
@@ -4,12 +4,22 @@ package sarama
 
 import "testing"
 
-var endTxnRequest = []byte{
-	0, 3, 't', 'x', 'n',
-	0, 0, 0, 0, 0, 0, 31, 64,
-	0, 1,
-	1,
-}
+var (
+	endTxnRequest = []byte{
+		0, 3, 't', 'x', 'n',
+		0, 0, 0, 0, 0, 0, 31, 64,
+		0, 1,
+		1,
+	}
+
+	endTxnRequestV3 = []byte{
+		4, 't', 'x', 'n', // TransactionalId
+		0, 0, 0, 0, 0, 0, 31, 64, // ProducerId
+		0, 1, // ProducerEpoch
+		1, // Committed
+		0, // tagged fields
+	}
+)
 
 func TestEndTxnRequest(t *testing.T) {
 	req := &EndTxnRequest{
@@ -19,5 +29,8 @@ func TestEndTxnRequest(t *testing.T) {
 		TransactionResult: true,
 	}
 
-	testRequest(t, "", req, endTxnRequest)
+	testRequest(t, "v0", req, endTxnRequest)
+
+	req.Version = 3
+	testRequest(t, "v3", req, endTxnRequestV3)
 }
